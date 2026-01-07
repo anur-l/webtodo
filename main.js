7975363fc1146ac5b8c7d4faec290a6d559df1a1
aj <amjamal4050@gmail>
2026-01-07 21:00:05 +0300

    adding ability to add todo and styling css

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const task = document.getElementsByClassName("tasks");
-const btnadd = document.getElementsByClassName("adding");
+const task = document.querySelector(".tasks");
+const btnadd = document.querySelector(".adding");
+const listt = document.querySelector("ul");
+btnadd.addEventListener("click", function() {
+  const userinput = task.value;
+  if (userinput !== "") {
+    const divlist = document.createElement("div");
+    divlist.style.display = "flex";
+    divlist.style.justifyContent = "space-between"; 
+    const newli = document.createElement("li");
+    newli.textContent = userinput;
+    newli.style.paddingRight = "4rem";
+    const delbtn = document.createElement("button");
+    delbtn.textContent = "x";
+    divlist.appendChild(newli);
+    divlist.appendChild(delbtn);
+    listt.appendChild(divlist);
+  }
+  task.value = "";
+});
