165f48714261acbabf47c76c6a1a637b7df0d12f
aj <amjamal4050@gmail>
2026-01-08 12:34:47 +0300

    adding line-through after fininshing task and changing theme

--- main.js
+++ main.js
@@ -6,16 +6,21 @@ function reading(userinput) {
   const divlist = document.createElement("div");
   divlist.style.display = "flex";
   divlist.style.justifyContent = "space-between";
+  divlist.classList.add("boxtodo");
+
   const divpara = document.createElement("div");
   divpara.style.display = "flex";
   divpara.style.gap = "0.4rem";
+
   const cbox = document.createElement("input");
   cbox.type = "checkbox";
-  
+  cbox.classList.add("cbox");
+
   const newp = document.createElement("p");
   newp.classList.add("todopara");
   newp.textContent = userinput;
   newp.style.paddingRight = "4rem";
+
   const delbtn = document.createElement("button");
   delbtn.classList.add("del");
   delbtn.textContent = "x";
@@ -25,6 +30,16 @@ function reading(userinput) {
   divlist.appendChild(divpara);
   divlist.appendChild(delbtn);
   listing.appendChild(divlist);
+  cbox.addEventListener("change", () => {
+    if (cbox.checked) {
+      newp.style.textDecoration = 'line-through';
+      newp.style.color = 'grey';
+    }else {
+    newp.style.textDecoration = 'none';
+    newp.style.color = 'black';
+  }
+  });
+  userinput.value = "";
 }
 window.onload = () => {
   const saveti = localStorage.getItem("todolist");
